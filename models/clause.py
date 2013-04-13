423934f4012d2ebfdbebb051bb7b3e906c4af1af
Silas Barta <sbarta@gmail.com>
2013-04-13 21:03:32 +0200

    added formula model and is_satisfied checks

--- models/clause.py
+++ models/clause.py
@@ -1,10 +1,10 @@
-from sys import stdout, stderr
+from sys import exit, stdout, stderr
 
 class Clause:
 
-    def __init__(self, var_indices, var_settings):
+    def __init__(self, var_indices, lit_settings):
         clause_len = len(var_indices) 
-        settings_len = len(var_settings)
+        settings_len = len(lit_settings)
         if clause_len != settings_len:
             stderr.write("Given {lits} literal(s) and {asgns} assignment(s);\n".format(
                 lits=clause_len,
@@ -14,7 +14,7 @@ class Clause:
         self.clause_state = {}
         for vari in range(clause_len):
             if vari < clause_len:
-                self.clause_state[var_indices[vari]] = bool(var_settings[vari])
+                self.clause_state[var_indices[vari]] = bool(lit_settings[vari])
             else:
                 self.clause_state[var_indices[vari]] = True
 
@@ -25,13 +25,34 @@ class Clause:
                 True: "+",
                 False: "~",
         }
-        for var_num, var_set in self.clause_state.iteritems():
+        for var_num, lit_set in self.clause_state.iteritems():
             stdout.write(" {sym}X{num} ".format(
-                sym=bool_to_sym[var_set],
+                sym=bool_to_sym[lit_set],
                 num=var_num,
             ))
         stdout.write(")\n")
 
 
-a = Clause([1,4,5],[1,0,1])
-a.write()
+    def show_vars_used(self):
+        print ' '.join([str(x) for x in self.clause_state])
+
+
+    def is_satisfied(self, var_stgs):
+        """Input: var_stgs, a dict mapping vars to truth assignments;\nkeys should match variable indices stored in clause; all unassigned vars are assumed true
+        """
+        for var_num, lit_set in self.clause_state.iteritems():
+            if var_num not in var_stgs:
+                stderr.write("WARNING: Variable {num} not assigned; assuming it is set to True.\n".format(num=var_num))
+                if lit_set == True:
+                    return True
+            else:
+                if lit_set == var_stgs[var_num]:
+                    return True
+        return False
+
+if __name__ == "__main__":
+    a = Clause([1,4,5],[1,0,1])
+    a.write()
+    print a.is_satisfied({1:True,4:False})
+    print a.is_satisfied({1:False,4:True,})
+    a.show_vars_used()
---
+++ models/formula.py
@@ -0,0 +1,28 @@
+from sys import stderr
+import clause
+
+class Formula:
+
+    def __init__(self, clauses=[]):
+        self.clauses = clauses
+
+
+    def is_satisfied(self, var_stgs):
+        return all([x.is_satisfied(var_stgs) for x in self.clauses])
+
+
+
+my_clauses = [
+        clause.Clause([1,4,5],[1,0,1]),
+        clause.Clause([2,3],[1,1]),
+]
+
+if __name__ == "__main__":
+    f1 = Formula(my_clauses)
+    print f1.is_satisfied({
+        1: True,
+        2: True,
+        3: True,
+        4: True,
+        5: True,
+    })
